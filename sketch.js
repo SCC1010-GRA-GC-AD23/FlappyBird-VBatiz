d03f0f4ee110a31791c0cd8ae9e60a1a3393ff10
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-10-18 22:52:53 +0000

    Agregamos pantalla de inicio

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 let imagenFondo
+let imagenInicio
+let imagenPersonaje
 let x = 0
 let posY = 100
 let velY = 3
+let estado = 0
+
 
 function preload() {
   // put preload code here
   imagenFondo = loadImage('./images/fondojuego.png')
+  imagenInicio = loadImage('./images/fondoinicio.png')
+  imagenPersonaje = loadImage('./images/bird.png')
 }
 
 function setup() {
@@ -14,28 +20,47 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
-  background(255)
-  //rect(100,100,200,200)
-  image(imagenFondo,x,0)
-  image(imagenFondo,x+imagenFondo.width,0)
-  x = x - 5
+  
+  if (estado === 1) {
+    // put drawing code here
+    background(255)
+    //rect(100,100,200,200)
+    image(imagenFondo,x,0)
+    image(imagenFondo,x+imagenFondo.width,0)
+    x = x - 5
 
-  //Moviendo al personaje
-  velY = velY + 1
-  posY = posY + velY
+    //Moviendo al personaje
+    velY = velY + 1
+    posY = posY + velY
+
+    //Revisando si el personaje se sale de la pantalla
+    if (posY > height || posY < 0) {
+      estado = 0
+    }
+
+    if (x < -imagenFondo.width) {
+      x = 0
+    }
+
+    textSize(34)
+    //text("👻",300,posY)
+    image(imagenPersonaje,300,posY, 50,50)
 
 
-  if (x < -imagenFondo.width) {
-    x = 0
+  } else { //Significa que estamos en la pantalla de inicio
+    background(0)
+    image(imagenInicio,0,0)
   }
-
-  textSize(34)
-  text("👻",300,posY)
-
 }
 
 
-function mouseClicked() {
-  velY = -15
+function mousePressed() {
+  if (estado === 0) {
+    estado = 1
+    x = 0
+    velY = 3
+    posY = 50
+  } else {
+    velY = -15
+  }
 }
